aa3b6ab72b82991c99b6882755f6e58dd6ee43cf
Zirui Song <zrsong@amazon.com>
2025-06-26 14:37:01 +0800

    Add a comment for DEFAULT_N_POSTINGS = -1
    Signed-off-by: Zirui Song <zrsong@amazon.com>

--- src/main/java/org/opensearch/neuralsearch/sparse/common/SparseConstants.java
+++ src/main/java/org/opensearch/neuralsearch/sparse/common/SparseConstants.java
@@ -14,7 +14,7 @@ public final class SparseConstants {
     public static final String ALGO_TRIGGER_DOC_COUNT_FIELD = "algo_trigger_doc_count";
 
     public final class Seismic {
-        public static final int DEFAULT_N_POSTINGS = -1;
+        public static final int DEFAULT_N_POSTINGS = -1; // Determine whether a user has given a specific value
         public static final float DEFAULT_SUMMARY_PRUNE_RATIO = 0.4f;
         public static final float DEFAULT_CLUSTER_RATIO = 0.1f;
         public static final int DEFAULT_ALGO_TRIGGER_DOC_COUNT = 1000000;
