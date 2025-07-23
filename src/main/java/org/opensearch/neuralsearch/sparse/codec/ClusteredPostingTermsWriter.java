0727accee5b442852a02648044d74edbff854d42
yuye-aws <yuyezhu@amazon.com>
2025-07-23 11:22:22 +0800

    Attach one todo
    Signed-off-by: yuye-aws <yuyezhu@amazon.com>

--- src/main/java/org/opensearch/neuralsearch/sparse/codec/ClusteredPostingTermsWriter.java
+++ src/main/java/org/opensearch/neuralsearch/sparse/codec/ClusteredPostingTermsWriter.java
@@ -84,6 +84,7 @@ public class ClusteredPostingTermsWriter extends PushPostingsWriterBase {
         float cluster_ratio = Float.parseFloat(fieldInfo.attributes().get(CLUSTER_RATIO_FIELD));
         int nPostings = Integer.parseInt(fieldInfo.attributes().get(N_POSTINGS_FIELD));
         float summaryPruneRatio = Float.parseFloat(fieldInfo.attributes().get(SUMMARY_PRUNE_RATIO_FIELD));
+        // TODO: attach a lucene reader in the following CacheGatedForwardIndexReader
         this.postingClustering = new PostingClustering(
             nPostings,
             new RandomClustering(
