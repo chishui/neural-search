8c01ae61918b17eedf70ec918b93e13792f78932
Zirui Song <zrsong@amazon.com>
2025-08-01 14:10:04 +0800

    temp store before merge
    Signed-off-by: Zirui Song <zrsong@amazon.com>

--- src/main/java/org/opensearch/neuralsearch/sparse/NeuralSparseIndexShard.java
+++ src/main/java/org/opensearch/neuralsearch/sparse/NeuralSparseIndexShard.java
@@ -6,7 +6,24 @@ package org.opensearch.neuralsearch.sparse;
 
 import lombok.Getter;
 import lombok.extern.log4j.Log4j2;
+import org.apache.lucene.index.FieldInfo;
+import org.apache.lucene.index.LeafReader;
+import org.apache.lucene.index.LeafReaderContext;
+import org.apache.lucene.index.SegmentReader;
+import org.apache.lucene.store.Directory;
+import org.opensearch.common.lucene.Lucene;
+import org.opensearch.index.engine.Engine;
+import org.opensearch.index.mapper.MappedFieldType;
+import org.opensearch.index.mapper.MapperService;
 import org.opensearch.index.shard.IndexShard;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import static org.opensearch.knn.common.KNNConstants.VECTOR_DATA_TYPE_FIELD;
 
 /**
  * NeuralSparseIndexShard wraps IndexShard and adds methods to perform neural-sparse related operations against the shard
@@ -35,6 +52,74 @@ public class NeuralSparseIndexShard {
      */
     public String getIndexName() {
         return indexShard.shardId().getIndexName();
+    }
+
+    /**
+     * Load all the neural-sparse segments for this shard into the cache.
+     * First it tries to warm-up memory optimized fields, then load off-heap fields.
+     *
+     * @throws IOException Thrown when getting the SEISMIC Paths to be loaded in
+     */
+    public void warmup() throws IOException {
+        log.info("[Neural Sparse] Warming up index: [{}]", getIndexName());
+
+        final MapperService mapperService = indexShard.mapperService();
+        final String indexName = indexShard.shardId().getIndexName();
+        final Directory directory = indexShard.store().directory();
+
+        try (Engine.Searcher searcher = indexShard.acquireSearcher("neural-sparse-warmup-mem")) {
+            for (final LeafReaderContext leafReaderContext : searcher.getIndexReader().leaves()) {
+                // Load memory optimized searcher in a single segment first.
+                final Set<String> loadedFieldNames = warmUpMemoryOptimizedSearcher(leafReaderContext.reader(), mapperService, indexName);
+                log.info("[KNN] Loaded memory optimized searchers for fields {}", loadedFieldNames);
+
+                // Load off-heap index
+                final List<KNNIndexShard.EngineFileContext> engineFileContexts = getAllEngineFileContexts(loadedFieldNames, leafReaderContext);
+                warmUpOffHeapIndex(engineFileContexts, directory);
+                log.info("[KNN] Loaded off-heap indices for fields {}", engineFileContexts.stream().map(ctx -> ctx.fieldName));
+            }
+        }
+    }
+
+    private Set<String> warmUpMemoryOptimizedSearcher(
+            final LeafReader leafReader,
+            final MapperService mapperService,
+            final String indexName
+    ) {
+
+        final Set<FieldInfo> fieldsForMemoryOptimizedSearch = StreamSupport.stream(leafReader.getFieldInfos().spliterator(), false)
+                .filter(fieldInfo -> fieldInfo.attributes().containsKey(KNNVectorFieldMapper.KNN_FIELD))
+                .filter(fieldInfo -> {
+                    final MappedFieldType fieldType = mapperService.fieldType(fieldInfo.getName());
+
+                    if (fieldType instanceof KNNVectorFieldType knnFieldType) {
+                        return MemoryOptimizedSearchSupportSpec.isSupportedFieldType(knnFieldType, indexName);
+                    }
+
+                    return false;
+                })
+                .collect(Collectors.toSet());
+
+        final SegmentReader segmentReader = Lucene.segmentReader(leafReader);
+        for (final FieldInfo field : fieldsForMemoryOptimizedSearch) {
+            final String dataTypeStr = field.getAttribute(VECTOR_DATA_TYPE_FIELD);
+            if (dataTypeStr == null) {
+                continue;
+            }
+            try {
+                // Partial load Faiss index by triggering search.
+                final VectorDataType vectorDataType = VectorDataType.get(dataTypeStr);
+                if (vectorDataType == VectorDataType.FLOAT) {
+                    segmentReader.getVectorReader().search(field.getName(), (float[]) null, null, null);
+                } else {
+                    segmentReader.getVectorReader().search(field.getName(), (byte[]) null, null, null);
+                }
+            } catch (Exception e) {
+                // Ignore
+            }
+        }
+
+        return fieldsForMemoryOptimizedSearch.stream().map(FieldInfo::getName).collect(Collectors.toSet());
     }
 
 }
