e33effd9a1b43aa100bdf5e35d34caead16c3cfb
Liyun Xiu <xiliyun@amazon.com>
2025-08-26 15:28:14 +0800

    Add documentation
    Signed-off-by: Liyun Xiu <xiliyun@amazon.com>

--- src/main/java/org/opensearch/neuralsearch/sparse/cache/AccountableTracker.java
+++ src/main/java/org/opensearch/neuralsearch/sparse/cache/AccountableTracker.java
@@ -6,9 +6,18 @@ package org.opensearch.neuralsearch.sparse.cache;
 
 import org.apache.lucene.util.Accountable;
 
+/**
+ * Abstract base class for tracking RAM usage of objects.
+ * Provides functionality to record and report memory consumption.
+ */
 public abstract class AccountableTracker implements Accountable {
     private final RamBytesRecorder recorder = new RamBytesRecorder(0);
 
+    /**
+     * Records the number of bytes used by an object.
+     *
+     * @param bytes number of bytes to record
+     */
     public void recordUsedBytes(long bytes) {
         recorder.record(bytes);
     }
